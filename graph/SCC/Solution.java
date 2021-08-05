61be6bec87919c67003822c61cde07a0ab384f76
shivam gupta <mit2020003@iiita.ac.in>
2021-08-05 16:23:25 +0530

    hide dirs

--- graph/SCC/Solution.java
+++ graph/SCC/Solution.java
@@ -13,8 +13,8 @@ class Solution
         }
         int E = in.nextInt();
         for (int i = 0; i < E; i++){
-            int u = in.nextInt();
-            int v = in.nextInt();
+            int u = in.nextInt() - 1;
+            int v = in.nextInt() - 1;
             GT.get(u).add(v);
         }
 
@@ -39,14 +39,20 @@ class Solution
         dfs1(G, 0, iv, stack);
 
         Arrays.fill(iv, false);
-        for (int i = 0; i < V; i++){
+        int C = 0, D = 0;
+        while(!stack.isEmpty()){
             int u = stack.pop();
             if(!iv[u]){
-                count++;
-                dfs2(GT, u, iv);
+//                count++; //count number of components
+                int val = dfs2(GT, u, iv);
+                if(val % 2 == 0){
+                    D += val;
+                }else{
+                    C += val;
+                }
             }
         }
-        return count;
+        return C - D;
     }
 
     public void dfs1(ArrayList<ArrayList<Integer>> G, int u, boolean[] iv, Stack<Integer> stack){
@@ -59,12 +65,14 @@ class Solution
         stack.push(u);
     }
 
-    public void dfs2(ArrayList<ArrayList<Integer>> G, int u, boolean[] iv){
+    public int dfs2(ArrayList<ArrayList<Integer>> G, int u, boolean[] iv){
         iv[u] = true;
+        int count = 0;
         for (int v : G.get(u)){
             if(!iv[v]){
-                dfs2(G, v, iv);
+                count += dfs2(G, v, iv);
             }
         }
+        return count + 1;
     }
 }
